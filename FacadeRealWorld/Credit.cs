3b253f77b9308cd573df69f2102611417ce0d262
Roman Karpov <poma.05@mail.ru>
2020-02-06 13:17:56 +0400

    init

---
+++ FacadeRealWorld/Credit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadeRealWorld
+{
+    public class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+}
---
+++ FacadeRealWorld/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadeRealWorld
+{
+    public class Customer
+    {
+        private string name;
+        public Customer(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
